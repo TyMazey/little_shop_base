4db85d18165875648d7739623b392b718e7b9200
Ty Mazey <tymazey@gmail.com>
2019-02-24 20:19:52 -0700

    users can navigate around the site and the coupon they entered stays applied

--- spec/features/cart/cart_spec.rb
+++ spec/features/cart/cart_spec.rb
@@ -381,6 +381,29 @@ RSpec.describe 'cart workflow', type: :feature do
       expect(page).to have_content('Total: $3.00')
       expect(page).to have_content('Total After Discount: $3.00')
     end
+
+    scenario 'as a registered user coupons stay even when i leave the cart page' do
+      hundred_dollars_off = create(:coupon, coupon_type: 0, value: 100, user: @merchant)
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit profile_orders_path
+      expect(page).to have_content('You have no orders yet')
+
+      visit item_path(@item)
+      click_button "Add to Cart"
+      visit cart_path
+
+      within '.coupon-code' do
+        fill_in :coupon, with: 'Coupon Name 3'
+        click_button 'Add Coupon'
+      end
+      visit items_path
+      visit cart_path
+
+      expect(page).to have_content('Total: $3.00')
+      expect(page).to have_content('Total After Discount: $0')
+    end
   end
 end
 
